f14fd0183b44307c2c7c448cc630b7f69e6fa245
eastflag <dklee98@>
2021-04-30 15:41:15 +0900

    프로젝트 설정 - antd 커스터마이징 적용.

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              '@primary-color': '#1DA57A',
+              // '@layout-header-background': '#001529',
+              // '@layout-header-color': '#ffffff'
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
