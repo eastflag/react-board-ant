9180caae24369475fa6370c11e26838936de7ba7
eastflag <dklee98@>
2021-05-03 16:36:08 +0900

    라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.less';
 import {Layout} from "antd";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import BoardList from "./pages/board-list/BoardList";
+import BoardRegister from "./pages/board-register/BoardRegister";
+import BoardView from "./pages/board-view/BoardView";
+import BoardEdit from "./pages/board-edit/BoardEdit";
 
 const { Header, Footer, Content } = Layout;
 
@@ -8,7 +13,16 @@ function App() {
   return (
     <Layout>
       <Header>Header</Header>
-      <Content>Content</Content>
+      <Content>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={BoardList}></Route>
+            <Route path="/board-register" component={BoardRegister}></Route>
+            <Route path="/board-view/:id" component={BoardView}></Route>
+            <Route path="/board-edit/:id" component={BoardEdit}></Route>
+          </Switch>
+        </BrowserRouter>
+      </Content>
       <Footer>Footer</Footer>
     </Layout>
   );
