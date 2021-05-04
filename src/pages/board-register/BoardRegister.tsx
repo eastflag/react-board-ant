d7950249ebb532a365c330227a3dde3a29571583
eastflag <dklee98@>
2021-05-04 11:35:21 +0900

    게시판 등록 - ant 적용

--- src/pages/board-register/BoardRegister.tsx
+++ src/pages/board-register/BoardRegister.tsx
@@ -1,40 +1,58 @@
-import React, {useState} from 'react';
+import React from 'react';
 import axios from 'axios';
 import {Board} from "../../dto/Board";
+import {Button, Form, Input, message} from "antd";
 
 const BoardRegister: React.FC = (props: any) => {
-  const [validated, setValidated] = useState(false);
+  const [form] = Form.useForm();
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    event.stopPropagation();
+  const handleSubmit = (values: any) => {
+    console.log(values);
 
-    const form = event.currentTarget;
-    if (!form.checkValidity()) {
-      setValidated(false);
-      return;
-    }
-
-    setValidated(true);
-    // Form.Grou의 controlid는 control의 id를 생성 => form[id] => control 노드 로 접근
-    console.log(form.titleInput.value);
-    const board = {
-      title: form.titleInput.value,
-      content: form.contentText.value
-    }
-    addBoard(board);
+    addBoard(values);
   };
 
   const addBoard = async (board: Board) => {
     const res = await axios.post('/api/board', board);
     console.log(res);
 
-    props.history.push('/');
+    if (res.status >= 200 && res.status < 300) {
+      message.success('This is a success message');
+      props.history.push('/');
+    } else {
+      message.error('error happened.')
+    }
   }
 
   return (
     <>
-
+      <Form
+        name="boardForm"
+        layout="vertical"
+        form={form}
+        requiredMark={true}
+        onFinish={handleSubmit}
+      >
+        <Form.Item label="제목" name="title" rules={[
+          {
+            required: true,
+            message: '제목을 입력하세요',
+          }
+        ]}>
+          <Input />
+        </Form.Item>
+        <Form.Item label="내용" name="content" rules={[
+          {
+            required: true,
+            message: '내용을 입력하세요',
+          }
+        ]}>
+          <Input.TextArea rows={15} />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">Submit</Button>
+        </Form.Item>
+      </Form>
     </>
   );
 };
